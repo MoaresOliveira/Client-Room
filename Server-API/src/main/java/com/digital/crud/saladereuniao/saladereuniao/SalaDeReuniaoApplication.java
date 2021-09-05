7ac56c30faaff0664c929db39f6625701d245f11
MoaresOliveira <moaresoliveira@gmail.com>
2021-09-05 08:27:34 -0300

    First Commit

---
+++ Server-API/src/main/java/com/digital/crud/saladereuniao/saladereuniao/SalaDeReuniaoApplication.java
@@ -0,0 +1,15 @@
+package com.digital.crud.saladereuniao.saladereuniao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class SalaDeReuniaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalaDeReuniaoApplication.class, args);
+    }
+
+}
---
+++ Server-API/src/main/java/com/digital/crud/saladereuniao/saladereuniao/model/Room.java
@@ -0,0 +1,24 @@
+package com.digital.crud.saladereuniao.saladereuniao.model;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "meetingroom")
+@Data
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "date", nullable = false)
+    private String date;
+    @Column(name = "startHour", nullable = false)
+    private String startHour;
+    @Column(name = "endHour", nullable = false)
+    private String endHour;
+}
